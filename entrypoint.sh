e4745c1fb23a5ff8b720646b9edac9290fee17a8
Dave Horton <daveh@beachdognet.com>
2019-05-29 15:01:59 -0400

    entrypoint.sh - allow socket connections from anywhere

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'freeswitch' ]; then
-  shift
-
 # listen on all interfaces, allow connections from anywhere
 sed -i -e "s/name=\"listen-ip\" value=\".*\"/name=\"listen-ip\" value=\"0.0.0.0\"/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
-sed -i -e "s/<\!--<param name=\"apply-inbound-acl\" value=\"loopback.auto\"\/>-->/<param name=\"apply-inbound-acl\" value=\"loopback.auto\"\/>/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
+sed -i -e "s/<\!--<param name=\"apply-inbound-acl\" value=\"loopback.auto\"\/>-->/<param name=\"apply-inbound-acl\" value=\"socket_acl\"\/>/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
+sed -i -e "s/<\/network-lists>/<list name=\"socket_acl\" default=\"deny\"><node type=\"allow\" cidr=\"0.0.0.0\/0\"\/><\/list><\/network-lists>/g" /usr/local/freeswitch/conf/autoload_configs/acl.conf.xml
+
+if [ "$1" = 'freeswitch' ]; then
+  shift
 
 while :; do
   case $1 in 
