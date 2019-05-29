6e58fddc24d63b18a9da9dbc93a36ef6fbda766f
Dave Horton <daveh@beachdognet.com>
2019-05-29 13:56:45 -0400

    entrypoint.sh - modify event_socket to allow connections from LAN

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ set -e
 if [ "$1" = 'freeswitch' ]; then
   shift
 
+# listen on all interfaces, allow connections from anywhere
 sed -i -e "s/name=\"listen-ip\" value=\".*\"/name=\"listen-ip\" value=\"0.0.0.0\"/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
+sed -i -e "s/<\!--<param name=\"apply-inbound-acl\" value=\"loopback.auto\"\/>-->/<param name=\"apply-inbound-acl\" value=\"loopback.auto\"\/>/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
+
 while :; do
   case $1 in 
   -s|--sip-port)
