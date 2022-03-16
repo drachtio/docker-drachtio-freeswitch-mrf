324e7ae4df6550513db0c434fd5a9dfee4bcc0a6
Lyle Pratt <lylepratt@gmail.com>
2022-03-16 11:44:41 -0500

    Using digital ocean spaces instead of S3

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd
+#s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd
+s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd -ourl=https://nyc3.digitaloceanspaces.com -ouse_cache=/tmp
+
 
 # listen on all interfaces, allow connections from anywhere
 sed -i -e "s/name=\"listen-ip\" value=\".*\"/name=\"listen-ip\" value=\"0.0.0.0\"/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
