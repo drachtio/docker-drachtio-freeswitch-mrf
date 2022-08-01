0076ee4bb9d352da3505d8e20659cdc1f3242924
Lyle Pratt <lylepratt@gmail.com>
2022-08-01 16:27:48 -0500

    Updating S3 mount options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd
+s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd -o endpoint=us-east-2 -o url="https://s3-us-east-2.amazonaws.com"
 
 # listen on all interfaces, allow connections from anywhere
 sed -i -e "s/name=\"listen-ip\" value=\".*\"/name=\"listen-ip\" value=\"0.0.0.0\"/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
