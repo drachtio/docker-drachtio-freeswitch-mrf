abdb8a647d756cb2607636f3af60bf6ff8e76b3b
Dave Horton <daveh@beachdognet.com>
2024-08-26 08:46:21 -0400

    changes to build multi-arch

--- build-locally.sh
+++ build-locally.sh
@@ -6,6 +6,7 @@ if [ ! -f ".env" ]; then
   exit 1
 fi
 
+# Extract versions from the .env file
 cmakeVersion=$(grep cmakeVersion .env | awk -F '=' '{print $2}')
 grpcVersion=$(grep grpcVersion .env | awk -F '=' '{print $2}')
 libwebsocketsVersion=$(grep libwebsocketsVersion .env | awk -F '=' '{print $2}')
@@ -15,11 +16,19 @@ sofiaVersion=$(grep sofiaVersion .env | awk -F '=' '{print $2}')
 awsSdkCppVersion=$(grep awsSdkCppVersion .env | awk -F '=' '{print $2}')
 freeswitchModulesVersion=$(grep freeswitchModulesVersion .env | awk -F '=' '{print $2}')
 freeswitchVersion=$(grep freeswitchVersion .env | awk -F '=' '{print $2}')
-
 dockerImageRepo=$(grep dockerImageRepo .env | awk -F '=' '{print $2}')
 dockerImageVersion=$(grep dockerImageVersion .env | awk -F '=' '{print $2}')
 
-docker build \
+# Specify the image name and tag
+imageName="drachtio/drachtio-freeswitch-mrf"
+imageTag="latest"
+
+# Create and use a new Buildx builder instance (if not already done)
+docker buildx create --use --name mybuilder --bootstrap || docker buildx use mybuilder
+
+# Build and push multi-architecture images
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
   --build-arg CMAKE_VERSION="${cmakeVersion}" \
   --build-arg GRPC_VERSION="${grpcVersion}" \
   --build-arg LIBWEBSOCKETS_VERSION="${libwebsocketsVersion}" \
@@ -30,3 +39,6 @@ docker build \
   --build-arg FREESWITCH_MODULES_VERSION="${freeswitchModulesVersion}" \
   --build-arg FREESWITCH_VERSION="${freeswitchVersion}" \
   . --tag "${dockerImageRepo}:${dockerImageVersion}"
+
+# Optional: remove the builder after the build to clean up
+docker buildx rm mybuilder
